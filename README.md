d1be676c0c39a3844bd01e33399ea17e585cee6c
joaquimserafim <joaquim.serafim@gmail.com>
2016-04-08 17:32:11 +0100

    last changes
    add tap-spec to dev dependencies
    
    update Doc
    
    v2
    
    add security validation
    
    add bench
    
    rm bench modules from dev dependencies
    
    rm bench modules from dev dependencies

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ JWT encode and decode for Node.js that can use callbacks or by returning an obje
 
 **WIKI**
 
-JSON Web Token (JWT) is a compact URL-safe means of representing claims to be transferred between two parties. The claims in a JWT are encoded as a JavaScript Object Notation (JSON) object that is used as the payload of a JSON Web Signature (JWS) structure or as the plaintext of a JSON Web Encryption (JWE) structure, enabling the claims to be digitally signed or MACed and/or encrypted. 
+JSON Web Token (JWT) is a compact URL-safe means of representing claims to be transferred between two parties. The claims in a JWT are encoded as a JavaScript Object Notation (JSON) object that is used as the payload of a JSON Web Signature (JWS) structure or as the plaintext of a JSON Web Encryption (JWE) structure, enabling the claims to be digitally signed or MACed and/or encrypted.
+
 
 [info](http://tools.ietf.org/html/draft-ietf-oauth-json-web-token-08) & [more info](http://self-issued.info/docs/draft-jones-json-web-token-01.html)
 
@@ -14,30 +15,32 @@ JSON Web Token (JWT) is a compact URL-safe means of representing claims to be tr
 
 [![Build Status](https://img.shields.io/badge/build-passing-brightgreen.svg?style=flat-square)](https://travis-ci.org/joaquimserafim/json-web-token)![Code Coverage 100%](https://img.shields.io/badge/code%20coverage-100%25-green.svg?style=flat-square)[![ISC License](https://img.shields.io/badge/license-ISC-blue.svg?style=flat-square)](https://github.com/joaquimserafim/json-web-token/blob/master/LICENSE)
 
+the version `2.*.*` should work only for NodeJS >= **4** for NodeJS **0.10** and **0.12** should install the version `1.6.3`
 
-###API
-  
-  
-#####  jwt#encode(key, payload, [algorithm], cb)
-  
+### API
+
+
+##### jwt#encode(key, payload, [algorithm], cb)
+
 * **key**, your secret
-* **payload**, the payload or Claim Names, 
+* **payload**, the payload or Claim Names,
 
-	ex:
-	
-		{
-		  "iss": "my_issurer",
-		  "aud": "World",
-		  "iat": 1400062400223,
-		  "typ": "/online/transactionstatus/v2",
-		  "request": {
-		    "myTransactionId": "[myTransactionId]",
-		    "merchantTransactionId": "[merchantTransactionId]",
-		    "status": "SUCCESS"
-		  }
-		}
+*ex:*
+```js
+{
+   "iss": "my_issurer",
+  "aud": "World",
+  "iat": 1400062400223,
+  "typ": "/online/transactionstatus/v2",
+  "request": {
+    "myTransactionId": "[myTransactionId]",
+    "merchantTransactionId": "[merchantTransactionId]",
+    "status": "SUCCESS"
+  }
+}
+```
 
-	*attention that exists some reserved claim names (like "iss", "iat", etc..) check [in here](http://tools.ietf.org/html/draft-ietf-oauth-json-web-token-08#section-4) for more info about JWT Claims.*	
+*attention that exists some reserved claim names (like "iss", "iat", etc..) check [in here](http://tools.ietf.org/html/draft-ietf-oauth-json-web-token-08#section-4) for more info about JWT Claims.*
 * **algorithm**, default to 'sha256', use *jwt#getAlgorithms()* to get the supported algorithms
 * **cb**, the callback(err[name, message], token)
 
@@ -51,66 +54,82 @@ JSON Web Token (JWT) is a compact URL-safe means of representing claims to be tr
 
 #### Example
 
-	var jwt = require('json-web-token');
-	
-	var payload = {
-	  "iss": "my_issurer",
-	  "aud": "World",
-	  "iat": 1400062400223,
-	  "typ": "/online/transactionstatus/v2",
-	  "request": {
-	    "myTransactionId": "[myTransactionId]",
-	    "merchantTransactionId": "[merchantTransactionId]",
-	    "status": "SUCCESS"
-	  }
-	};
-	
-	var secret = 'TOPSECRETTTTT';
-	
-	// encode
-	jwt.encode(secret, payload, function (err, token) {
-	  if (err) {
-	  	return console.error(err.name, err.message);
-	  } else {
-	  	console.log(token);
+```js
+var jwt = require('json-web-token');
 
-		// decode
-	  	jwt.decode(secret, token, function (err_, decode) {
-	    	if (err) {
-	  			return console.error(err.name, err.message);
-	  		} else {
-	    		console.log(decode);
-	    	}
-	  	});
-	  }
-	});
+var payload = {
+  "iss": "my_issurer",
+  "aud": "World",
+  "iat": 1400062400223,
+  "typ": "/online/transactionstatus/v2",
+  "request": {
+    "myTransactionId": "[myTransactionId]",
+    "merchantTransactionId": "[merchantTransactionId]",
+    "status": "SUCCESS"
+  }
+};
 
+var secret = 'TOPSECRETTTTT';
+
+// encode
+jwt.encode(secret, payload, function (err, token) {
+  if (err) {
+    return console.error(err.name, err.message);
+  } else {
+    console.log(token);
+
+    // decode
+    jwt.decode(secret, token, function (err_, decode) {
+      if (err) {
+        return console.error(err.name, err.message);
+      } else {
+        console.log(decode);
+      }
+    });
+  }
+});
+```
 
 ---
 
 #####this projet has been set up with a precommit that forces you to follow a code style, no jshint issues and 100% of code coverage before commit
 
 to run test
-``` js
+```js
 npm test
 ```
 
 to run jshint
-``` js
-npm run jshint
+```js
+npm run lint
 ```
 
 to run code style
-``` js
-npm run code-style
+```js
+npm run style
 ```
 
-to run check code coverage
-``` js
-npm run check-coverage
+to run code coverage
+```js
+npm run coverage
 ```
 
 to open the code coverage report
-``` js
-npm run open-coverage
+```js
+npm run coverage:open
+```
+
+to run benchmarks
+```js
+npm run bench
+```
+
+to run the source complexity tool
+```js
+npm run complexity
+```
+
+to open the complexity report
+```js
+npm run complexity:open
 ```
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-var read    = require('fs').readFileSync;
-var test    = require('tape');
-var b64url  = require('base64-url');
-var jwt     = require('../.');
+var read    = require('fs').readFileSync
+var test    = require('tape')
+var b64url  = require('base64-url')
+var jwt     = require('../.')
 
 var payload = {
   iss: 'my_issurer',
@@ -15,174 +15,174 @@ var payload = {
     merchantTransactionId: '[merchantTransactionId]',
     status: 'SUCCESS'
   }
-};
+}
 
-var secret = 'TOPSECRETTTTT';
-var theToken = null;
-var theTokenSign = null;
-var algorithms;
+var secret = 'TOPSECRETTTTT'
+var theToken = null
+var theTokenSign = null
+var algorithms
 
 test('get the error class', function(assert) {
-  var JWTError = jwt.JWTError;
-  assert.equal(typeof JWTError, 'function');
-  assert.ok((new JWTError()) instanceof Error);
-  assert.end();
-});
+  var JWTError = jwt.JWTError
+  assert.equal(typeof JWTError, 'function')
+  assert.ok((new JWTError()) instanceof Error)
+  assert.end()
+})
 
 test('get the jwt supported algorithms', function(assert) {
-  algorithms = jwt.getAlgorithms();
-  assert.deepEqual(typeof algorithms, 'object');
-  assert.ok(Object.keys(algorithms).length);
-  assert.end();
-});
+  algorithms = jwt.getAlgorithms()
+  assert.deepEqual(typeof algorithms, 'object')
+  assert.ok(Object.keys(algorithms).length)
+  assert.end()
+})
 
 test('jwt - encode with callback / hmac', function(assert) {
   jwt.encode(secret, payload, function(err, token) {
-    assert.deepEqual(err, undefined);
-    assert.ok(token);
-    assert.deepEqual(token.split('.').length, 3);
-    theToken = token;
-    assert.end();
-  });
-});
+    assert.deepEqual(err, null)
+    assert.ok(token)
+    assert.deepEqual(token.split('.').length, 3)
+    theToken = token
+    assert.end()
+  })
+})
 
 test('jwt - encode with callback / sign', function(assert) {
-  var pem = read(__dirname + '/fixtures/test.pem').toString('ascii');
+  var pem = read(__dirname + '/fixtures/test.pem').toString('ascii')
   jwt.encode(pem, payload, 'RS256', function(err, token) {
-    assert.deepEqual(err, undefined);
-    assert.ok(token);
-    theTokenSign = token;
-    assert.deepEqual(token.split('.').length, 3);
-    assert.end();
-  });
-});
+    assert.deepEqual(err, null)
+    assert.ok(token)
+    theTokenSign = token
+    assert.deepEqual(token.split('.').length, 3)
+    assert.end()
+  })
+})
 
 test('jwt - encode with callback / bad algorithm', function(assert) {
   jwt.encode(secret, payload, 'wow', function(err) {
-    assert.deepEqual(err.message, 'The algorithm is not supported!');
-    assert.end();
-  });
-});
+    assert.deepEqual(err.message, 'The algorithm is not supported!')
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / hmac', function(assert) {
   jwt.decode(secret, theToken, function(err, decodePayload) {
-    assert.deepEqual(err, undefined);
-    assert.deepEqual(decodePayload, payload);
-    assert.end();
-  });
-});
+    assert.deepEqual(err, null)
+    assert.deepEqual(decodePayload, payload)
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / sign', function(assert) {
-  var crt = read(__dirname + '/fixtures/test.crt').toString('ascii');
+  var crt = read(__dirname + '/fixtures/test.crt').toString('ascii')
   jwt.decode(crt, theTokenSign, function(err, decodePayload) {
-    assert.deepEqual(err, undefined);
-    assert.deepEqual(decodePayload, payload);
-    assert.end();
-  });
-});
+    assert.deepEqual(err, null)
+    assert.deepEqual(decodePayload, payload)
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / bad algorithm', function(assert) {
-  var t = theToken.split('.').slice(1, 3);
-  var badHeader = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJ3b3cifQ';
-  t = badHeader + '.' + t.join('.');
+  var t = theToken.split('.').slice(1, 3)
+  var badHeader = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJ3b3cifQ'
+  t = badHeader + '.' + t.join('.')
   jwt.decode(secret, t, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The algorithm is not supported!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The algorithm is not supported!')
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / invalid key', function(assert) {
   jwt.decode('wow', theToken, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'Invalid key!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'Invalid key!')
+    assert.end()
+  })
+})
 
 test('jwt - encode with callback / null payload', function(assert) {
   jwt.encode(secret, null, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The key and payload are mandatory!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The key and payload are mandatory!')
+    assert.end()
+  })
+})
 
 test('jwt - encode with callback / empty payload', function(assert) {
   jwt.encode(secret, {}, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The payload is empty object!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The payload is an empty object!')
+    assert.end()
+  })
+})
 
 test('jwt - encode with callback / null secret', function(assert) {
   jwt.encode(null, payload, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The key and payload are mandatory!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The key and payload are mandatory!')
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / null key', function(assert) {
   jwt.decode(null, theToken, function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The key and token are mandatory!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The key and token are mandatory!')
+    assert.end()
+  })
+})
 
 test('jwt - decode with callback / bad token', function(assert) {
   jwt.decode(secret, theToken.split('.').slice(0, 2).join('.'), function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The JWT should consist of three parts!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The JWT should consist of three parts!')
+    assert.end()
+  })
+})
 
 //
 // without callback but returning the result
 //
 test('jwt - encode without callback / hmac', function(assert) {
-  var res = jwt.encode(secret, payload);
-  assert.deepEqual(typeof res, 'object');
-  assert.deepEqual(res.error, undefined);
-  assert.ok(res.value);
-  assert.deepEqual(res.value.split('.').length, 3);
-  assert.end();
-});
+  var res = jwt.encode(secret, payload)
+  assert.deepEqual(typeof res, 'object')
+  assert.deepEqual(res.error, null)
+  assert.ok(res.value)
+  assert.deepEqual(res.value.split('.').length, 3)
+  assert.end()
+})
 
 test('jwt - decode without callback / hmac', function(assert) {
-  var res = jwt.decode(secret, theToken);
-  assert.deepEqual(typeof res, 'object');
-  assert.deepEqual(res.error, undefined);
-  assert.deepEqual(res.value, payload);
-  assert.end();
-});
+  var res = jwt.decode(secret, theToken)
+  assert.deepEqual(typeof res, 'object')
+  assert.deepEqual(res.error, null)
+  assert.deepEqual(res.value, payload)
+  assert.end()
+})
 
 test('jwt - encode without callback / null payload', function(assert) {
-  var res = jwt.encode(secret, null);
-  assert.deepEqual(typeof res, 'object');
-  assert.equal(res.error.name, 'JWTError');
-  assert.equal(res.error.message, 'The key and payload are mandatory!');
-  assert.end();
-});
+  var res = jwt.encode(secret, null)
+  assert.deepEqual(typeof res, 'object')
+  assert.equal(res.error.name, 'JWTError')
+  assert.equal(res.error.message, 'The key and payload are mandatory!')
+  assert.end()
+})
 
 test('jwt - encode without callback / empty payload', function(assert) {
-  var res = jwt.encode(secret, {});
-  assert.deepEqual(typeof res, 'object');
-  assert.equal(res.error.name, 'JWTError');
-  assert.equal(res.error.message, 'The payload is empty object!');
-  assert.end();
-});
+  var res = jwt.encode(secret, {})
+  assert.deepEqual(typeof res, 'object')
+  assert.equal(res.error.name, 'JWTError')
+  assert.equal(res.error.message, 'The payload is an empty object!')
+  assert.end()
+})
 
 test('jwt - encode without callback / null secret', function(assert) {
-  var res = jwt.encode(null, payload);
-  assert.deepEqual(typeof res, 'object');
-  assert.equal(res.error.name, 'JWTError');
-  assert.equal(res.error.message, 'The key and payload are mandatory!');
-  assert.end();
-});
+  var res = jwt.encode(null, payload)
+  assert.deepEqual(typeof res, 'object')
+  assert.equal(res.error.name, 'JWTError')
+  assert.equal(res.error.message, 'The key and payload are mandatory!')
+  assert.end()
+})
 
 //
 // test the jwt vulnerability because of the "none" algorithm
@@ -192,20 +192,21 @@ test('jwt - encode without callback / null secret', function(assert) {
 
 test('should not encode for the "none" algorithm', function(assert) {
   jwt.encode(secret, payload, 'none', function(err) {
-    assert.equal(err.name, 'JWTError');
-    assert.equal(err.message, 'The algorithm is not supported!');
-    assert.end();
-  });
-});
+    assert.equal(err.name, 'JWTError')
+    assert.equal(err.message, 'The algorithm is not supported!')
+    assert.end()
+  })
+})
 
 test('should not decode for the "none" algorithm', function(assert) {
-  var encode    = jwt.encode(secret, payload).value;
-  var badToken  = encode.split('.');
-  var badAlg    = b64url.encode(JSON.stringify({typ: 'JWT', alg: 'none'}));
-  badToken[0]   = badAlg;
-  var result    = jwt.decode(secret, badToken.join('.'));
-  assert.deepEqual(!!result.error, true);
-  assert.equal(result.error.name, 'JWTError');
-  assert.equal(result.error.message, 'The algorithm is not supported!');
-  assert.end();
-});
+  var encode    = jwt.encode(secret, payload).value
+  var badToken  = encode.split('.')
+  var badAlg    = b64url.encode(JSON.stringify({typ: 'JWT', alg: 'none'}))
+  badToken[0]   = badAlg
+  var result    = jwt.decode(secret, badToken.join('.'))
+  assert.deepEqual(!!result.error, true)
+  assert.equal(result.error.name, 'JWTError')
+  assert.equal(result.error.message, 'The algorithm is not supported!')
+  assert.end()
+})
+
